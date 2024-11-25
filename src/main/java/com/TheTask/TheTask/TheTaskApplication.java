9ad818496ff0b8220e519974171a6a87d07d668f
Adam Nordgren <adno96@gmail.com>
2024-11-25 18:55:51 +0100

    init commit

---
+++ src/main/java/com/TheTask/TheTask/TheTaskApplication.java
@@ -0,0 +1,13 @@
+package com.TheTask.TheTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheTask/TheTask/TheTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheTask.TheTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
