63c25f8dfb210bead067a063bceab96b52dad1b1
Your Namejonas <skonby_jonas@hotmail.com>
2024-11-27 19:52:01 +0100

    created html for new user

--- src/main/java/com/TheTask/TheTask/UserController.java
+++ src/main/java/com/TheTask/TheTask/UserController.java
@@ -3,6 +3,25 @@ package com.TheTask.TheTask;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
 public class UserController {
-    List<User> users = new ArrayList<>();
+
+    @GetMapping("/create_user")
+    public String getCreateUserPage(){
+
+        return "create_user";
+    }
+
+    @PostMapping("/newUser")
+    public String newUser(@RequestParam("name") String name){
+
+        return "create_user";
+    }
+    
 }
