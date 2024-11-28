bc3257dddaa3e56c17e7db492618b010306c530a
Your Namejonas <skonby_jonas@hotmail.com>
2024-11-28 23:53:46 +0100

    created function so every current users todolist list in the fronted

--- src/main/java/com/TheTask/TheTask/TodoListController.java
+++ src/main/java/com/TheTask/TheTask/TodoListController.java
@@ -2,6 +2,7 @@ package com.TheTask.TheTask;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,12 +12,15 @@ public class TodoListController {
     private UserManager userManager;
 
     @PostMapping("/addToDoList")
-    public String addTodoList(@RequestParam String todoListTitle){
+    public String addTodoList(@RequestParam String todoListTitle, Model model){ //<-- Model model skicka data från controller till vyn
         User currentUser = userManager.getCurrentUser();
         TodoList newTodoList = new TodoList(todoListTitle);
         currentUser.addList(newTodoList);
-
         System.out.println("Antal listor för denna användare: " + currentUser.userOwnedLists.size());
+
+        // lägg till aktuella användaren och deras todolist i modellen
+        model.addAttribute("currnetUser", currentUser);
+        model.addAttribute("userOwnedLists", currentUser.getUserOwnedLists());
 
         return "redirect:/";
     }
