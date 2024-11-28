c573e2f386b91658a3d232d0c9f8fc628e26726c
Felicia Karlsson <felizia02@icloud.com>
2024-11-29 00:55:11 +0100

    felhantering vid inmatning av ny lista utan att user är vald

--- src/main/java/com/TheTask/TheTask/TodoListController.java
+++ src/main/java/com/TheTask/TheTask/TodoListController.java
@@ -2,6 +2,7 @@ package com.TheTask.TheTask;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,12 +12,17 @@ public class TodoListController {
     private UserManager userManager;
 
     @PostMapping("/addToDoList")
-    public String addTodoList(@RequestParam String todoListTitle){
+    public String addTodoList(@RequestParam String todoListTitle, Model model){
         User currentUser = userManager.getCurrentUser();
+        if (currentUser == null) {
+            model.addAttribute("errorMessage", "Ingen användare vald");
+            System.out.println("Måste välja användare först");
+        return "redirect:/";
+        } 
         TodoList newTodoList = new TodoList(todoListTitle);
         currentUser.addList(newTodoList);
-
-        System.out.println("Antal listor för denna användare: " + currentUser.userOwnedLists.size());
+       
+         System.out.println("Antal listor för denna användare: " + currentUser.userOwnedLists.size());
 
         return "redirect:/";
     }
